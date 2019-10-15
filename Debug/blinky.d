48d53cbb9bc927ad92d6c7709ffbd7394a00b011
Ahmed-Gaber-A <ahmedgaber3395@gmail.com>
2019-10-15 22:32:12 +0200

    first release

---
+++ Debug/blinky.d
@@ -0,0 +1,49 @@
+# FIXED
+
+blinky.obj: ../blinky.c
+blinky.obj: C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h
+blinky.obj: C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h
+blinky.obj: C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h
+blinky.obj: C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h
+blinky.obj: C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h
+blinky.obj: C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h
+blinky.obj: C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h
+blinky.obj: C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h
+blinky.obj: C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdbool.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_uart.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
+blinky.obj: C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdarg.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_can.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/can.h
+
+../blinky.c: 
+C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h: 
+C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h: 
+C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h: 
+C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h: 
+C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h: 
+C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h: 
+C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h: 
+C:/ti/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdarg.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_can.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/can.h: 
